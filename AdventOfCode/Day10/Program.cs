f5f576117787e969e088a3bfd2c7b21606088c60
Matt Connolly <themattconnolly@gmail.com>
2022-12-11 11:21:49 -0500

    day 10 ⭐

---
+++ AdventOfCode/Day10/Program.cs
@@ -0,0 +1,46 @@
+﻿using System.Text.RegularExpressions;
+
+namespace Day10
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Day 10!");
+
+            Regex addxRegex = new Regex(@"addx (-*\d+)");
+            int cycle = 1;
+            int x = 1;
+            int signalStrengthSum = 0;
+            foreach (string line in File.ReadLines("input.txt"))
+            {
+                if(line.Contains("noop"))
+                {
+                    cycle++;
+                }
+                else
+                {
+                    int addX = int.Parse(addxRegex.Match(line).Groups[1].Value);
+                    cycle++;
+
+                    if(cycle % 40 == 20)
+                    {
+                        signalStrengthSum += (cycle * x);
+                    }
+
+                    x += addX;
+                    cycle++;
+                }
+
+                if (cycle % 40 == 20)
+                {
+                    signalStrengthSum += (cycle * x);
+                }
+            }
+
+            Console.WriteLine("Cycles: " + cycle);
+            Console.WriteLine("X: " + x);
+            Console.WriteLine("Signal Strength Sum: " + signalStrengthSum);
+        }
+    }
+}
---
+++ AdventOfCode/Day9/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace Day9
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Day 9!");
+
+
+        }
+    }
+}
