fa8944db81262d9c6b13e7355836e32f4d87814b
Matt Connolly <themattconnolly@gmail.com>
2022-12-11 12:05:47 -0500

    day 10 ⭐⭐

--- AdventOfCode/Day10/Program.cs
+++ AdventOfCode/Day10/Program.cs
@@ -9,38 +9,79 @@ namespace Day10
             Console.WriteLine("Day 10!");
 
             Regex addxRegex = new Regex(@"addx (-*\d+)");
-            int cycle = 1;
+            int cycle = 0;
             int x = 1;
             int signalStrengthSum = 0;
+            List<char[]> screen = new List<char[]>() { };
+            screen.Add(new char[40]);
+            int row = 0;
+            int position = 0;
             foreach (string line in File.ReadLines("input.txt"))
             {
-                if(line.Contains("noop"))
+                
+                cycle++;
+
+                if (cycle % 40 == 20)
                 {
-                    cycle++;
+                    signalStrengthSum += (cycle * x);
+                }
+
+                // drawPositionOfSprit
+                UpdateScreen(screen, x, row, position++);
+                if (position == 40)
+                {
+                    position = 0;
+                    row++;
+                    screen.Add(new char[40]);
+                }
+
+                if (line.Contains("noop"))
+                {
+                    // do nothing   
                 }
                 else
                 {
                     int addX = int.Parse(addxRegex.Match(line).Groups[1].Value);
                     cycle++;
 
-                    if(cycle % 40 == 20)
+                    if (cycle % 40 == 20)
                     {
                         signalStrengthSum += (cycle * x);
                     }
 
-                    x += addX;
-                    cycle++;
-                }
+                    UpdateScreen(screen, x, row, position++);
+                    if (position == 40)
+                    {
+                        position = 0;
+                        row++;
+                        screen.Add(new char[40]);
+                    }
 
-                if (cycle % 40 == 20)
-                {
-                    signalStrengthSum += (cycle * x);
+                    x += addX;
                 }
             }
 
             Console.WriteLine("Cycles: " + cycle);
             Console.WriteLine("X: " + x);
             Console.WriteLine("Signal Strength Sum: " + signalStrengthSum);
+            Console.WriteLine("Screen:");
+
+            foreach(char[] screenRow in screen)
+            {
+                Console.WriteLine(screenRow);
+            }
+        }
+
+        internal static void UpdateScreen(List<char[]> screen, int x, int row, int position)
+        {
+            if(position >= x - 1 && position <= x + 1)
+            {
+                screen[row][position] = '#';
+            }
+            else
+            {
+                screen[row][position] = '.';
+            }
         }
     }
 }
