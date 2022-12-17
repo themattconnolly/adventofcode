37071ce346cdd4c63791a35c98935ec3d8a4bd41
Matt Connolly <themattconnolly@gmail.com>
2022-12-17 12:24:15 -0500

    day 14 ⭐

---
+++ AdventOfCode/Day13/Program.cs
@@ -0,0 +1,99 @@
+﻿using System.Runtime.Serialization.Formatters;
+using System.Text.RegularExpressions;
+
+namespace Day13
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Day 13!");
+
+            int orderedPairIndexTotal = 0;
+            int pairIndex = 0;
+            string[] lines = File.ReadAllLines("input.txt");
+            for(int lineNumber = 0; lineNumber < lines.Length; lineNumber+=3)
+            {
+                pairIndex++;
+                // parse left
+                Packet leftPacket = new Packet(lines[lineNumber]);
+                // parse right
+                Packet rightPacket = new Packet(lines[lineNumber + 1]);
+
+                // first pair
+                if (leftPacket.CompareTo(rightPacket) > 0)
+                {
+                    orderedPairIndexTotal += pairIndex;
+                }
+                else
+                {
+                    // not ordered
+                }
+            }
+
+            Console.WriteLine("Ordered pair index total: " + orderedPairIndexTotal);
+        }
+
+
+        internal class Packet : IComparable<Packet>
+        {
+            internal Packet(string value)
+            {
+                Match match = Regex.Match(value, @"\[(.*)\]");
+                if (match.Success)
+                {
+                    foreach(char c in match.Groups[1].Value)
+                    {
+
+                    }
+                    //Match sublistMatch = Regex.Match(, @"\[(.*)\]");
+                    //foreach (string subvalue in match.Groups[1].Value.Split(','))
+                    //{
+                    //    Packets.Add(new Packet(subvalue));
+                    //}
+                }
+
+                this.Value = value;
+            }
+
+            internal string Value;
+
+            internal List<Packet> Packets = new List<Packet>();
+
+            public int CompareTo(Packet? other)
+            {
+                if(Packets.Count > other.Packets.Count)
+                {
+                    return -1;
+                }
+
+                for (int i = 0; i < Packets.Count; i++)
+                {
+                    if(int.TryParse(Packets[i].Value, out int intA))
+                    {
+                        if(int.TryParse(other.Packets[i].Value, out int intB))
+                        {
+                            if(intA > intB)
+                            {
+                                // not valid
+                                return -1;
+                            }
+                        }
+                        else
+                        {
+                            // pretend A is a list
+                        }
+                    }
+                }
+                return 1;
+            }
+
+            internal void ParseValue()
+            {
+
+            }
+
+
+        }
+    }
+}
---
+++ AdventOfCode/Day14/Program.cs
@@ -0,0 +1,184 @@
+﻿using System.Drawing;
+using System.Text.RegularExpressions;
+
+namespace Day14
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Day 14!");
+
+            string[] lines = File.ReadAllLines("input.txt");
+            List<Point> allRocks = new List<Point>();
+
+            foreach (string line in lines)
+            {
+                List<Point> rockPoints = new List<Point>();
+                foreach (Match match in Regex.Matches(line, @"(\d*),(\d*)"))
+                {
+                    rockPoints.Add(new Point(int.Parse(match.Groups[1].Value), int.Parse(match.Groups[2].Value)));
+                }
+
+                Point? previousPoint = null;
+                foreach (Point point in rockPoints)
+                {
+                    if (previousPoint != null)
+                    {
+                        int previousX = previousPoint.Value.X;
+                        int previousY = previousPoint.Value.Y;
+                        int nextX = point.X;
+                        int nextY = point.Y;
+
+                        if (previousX < nextX)
+                        {
+                            for (int i = previousX; i <= nextX; i++)
+                            {
+                                if (previousY < nextY)
+                                {
+                                    for (int j = previousY; j <= nextY; j++)
+                                    {
+                                        allRocks.Add(new Point { X = i, Y = j });
+                                    }
+                                }
+                                else
+                                {
+                                    for (int j = previousY; j >= nextY; j--)
+                                    {
+                                        allRocks.Add(new Point { X = i, Y = j });
+                                    }
+                                }
+                            }
+                        }
+                        else
+                        {
+                            for (int i = previousX; i >= nextX; i--)
+                            {
+                                if (previousY <= nextY)
+                                {
+                                    for (int j = previousY; j <= nextY; j++)
+                                    {
+                                        if (allRocks.Exists(r => r.X == i && r.Y == j) == false)
+                                        {
+                                            allRocks.Add(new Point { X = i, Y = j });
+                                        }
+                                    }
+                                }
+                                else
+                                {
+                                    for (int j = previousY; j >= nextY; j--)
+                                    {
+                                        if (allRocks.Exists(r => r.X == i && r.Y == j) == false)
+                                        {
+                                            allRocks.Add(new Point { X = i, Y = j });
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                    }
+
+                    previousPoint = point;
+                }
+            }
+
+            int minX = allRocks.Min(r => r.X);
+            int minY = 0;
+            int maxX = allRocks.Max(r => r.X);
+            int maxY = allRocks.Max(r => r.Y);
+
+            List<Point> allSand = new List<Point>();
+            Point nextSand = NextSandMove(new Point(500, 0), allRocks, allSand);
+
+            while(nextSand.Y <= maxY)
+            {
+                // have not yet reached infinity
+                Point previousSand = Point.Empty;
+                while(nextSand.IsEmpty == false && nextSand.Y <= maxY)
+                {
+                    previousSand = nextSand;
+                    nextSand = NextSandMove(nextSand, allRocks, allSand);
+                }
+
+                if(nextSand.Y > maxY)
+                {
+                    continue;
+                }
+
+                // sand cannot move further, drop a new grain
+                allSand.Add(previousSand);
+                nextSand = NextSandMove(new Point(500, 0), allRocks, allSand);
+            }
+
+
+            // draw grid
+            for (int y = minY; y <= maxY; y++)
+            {
+                for (int x = minX; x <= maxX; x++)
+                {
+                    if (y == 0 && x == 500)
+                    {
+                        Console.Write("+");
+                        continue;
+                    }
+
+                    Point rock = allRocks.FirstOrDefault(r => r.X == x && r.Y == y);
+                    if (rock.IsEmpty)
+                    {
+                        Point sand = allSand.FirstOrDefault(s => s.X == x && s.Y == y);
+                        if (sand.IsEmpty)
+                        {
+                            Console.Write(".");
+                        }
+                        else
+                        {
+                            Console.Write("o");
+                        }
+                    }
+                    else
+                    {
+                        Console.Write("#");
+                    }
+                }
+
+                Console.WriteLine();
+            }
+
+            Console.WriteLine(allSand.Count + " grains of sand");
+        }
+
+        internal static Point NextSandMove(Point currentSand, List<Point> allRocks, List<Point> allSand)
+        {
+            Point nextMove = Point.Empty;
+            int nextMoveX = currentSand.X;
+            int nextMoveY = currentSand.Y + 1;
+
+            if(allRocks.FirstOrDefault(r => r.X == nextMoveX && r.Y == nextMoveY).IsEmpty &&
+                allSand.FirstOrDefault(s => s.X == nextMoveX && s.Y == nextMoveY).IsEmpty)
+            {
+                nextMove = new Point(nextMoveX, nextMoveY);
+            }
+            else
+            {
+                nextMoveX = nextMoveX - 1; // try to the left
+
+                if (allRocks.FirstOrDefault(r => r.X == nextMoveX && r.Y == nextMoveY).IsEmpty &&
+                allSand.FirstOrDefault(s => s.X == nextMoveX && s.Y == nextMoveY).IsEmpty)
+                {
+                    nextMove = new Point(nextMoveX, nextMoveY);
+                }
+                else
+                {
+                    nextMoveX = nextMoveX + 2; // try to the right
+                    if (allRocks.FirstOrDefault(r => r.X == nextMoveX && r.Y == nextMoveY).IsEmpty &&
+                        allSand.FirstOrDefault(s => s.X == nextMoveX && s.Y == nextMoveY).IsEmpty)
+                    {
+                        nextMove = new Point(nextMoveX, nextMoveY);
+                    }
+                }
+            }
+
+            return nextMove;
+        }
+    }
+}
