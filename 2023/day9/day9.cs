21a4f4ae50d92a96f6ba26fb021d255d21dfb69f
Matt Connolly <themattconnolly@gmail.com>
2023-12-09 09:26:17 -0500

    2023 day 9 part 2 ⭐

--- 2023/day9/day9.cs
+++ 2023/day9/day9.cs
@@ -89,7 +89,62 @@ public class Day9
         return nextNumber;
     }
 
-    private static string Instructions = "";
+    private static int DeterminePreviousNumber(int[] sequence)
+    {
+        int nextNumber = 0;
+
+        List<int[]> currentSequences = new List<int[]>();
+        currentSequences.Add(sequence);
+        int[] currentSequence = sequence;
+
+        while(currentSequence.Any(n => n != 0))
+        {
+            // create a new sequence that is populated with the differences between subsequent numbers
+            int[] newSequence = new int[currentSequence.Length - 1];
+            for(int i = 0; i < currentSequence.Length - 1; i++)
+            {
+                newSequence[i] = currentSequence[i + 1] - currentSequence[i];
+            }
+            currentSequence = newSequence;
+            currentSequences.Add(currentSequence);
+        }
+
+        // write out all the previous sequences
+        // foreach(int[] seq in previousSequences)
+        // {
+        //     Console.WriteLine("Previous sequence: " + string.Join(',', seq));
+        // }
+
+        // go back up the chain and add the last number in each sequence to the next number
+        for(int i = currentSequences.Count - 1; i >= 0; i--)
+        {
+            int[] newSequence = new int[currentSequences[i].Length + 1];
+            // copy the old sequence into the new one
+            for(int j = 0; j < currentSequences[i].Length; j++)
+            {
+                newSequence[j+1] = currentSequences[i][j];
+            }
+
+            int newFirstNumber = 0;
+            if(i != currentSequences.Count - 1)
+            {
+                newFirstNumber = currentSequences[i][0] - currentSequences[i + 1][0];
+            }
+            
+            newSequence[0] = newFirstNumber;
+            currentSequences[i] = newSequence;
+        }
+
+        // write out all the sequences
+        foreach(int[] seq in currentSequences)
+        {
+            Console.WriteLine("After: " + string.Join(',', seq));
+        }
+
+        nextNumber = currentSequences[0][0];
+
+        return nextNumber;
+    }
     public static void RunPart1()
     {
         ParseFile();
@@ -104,11 +159,23 @@ public class Day9
         }
 
         Console.WriteLine("Part 1 : " + part1sum);
-        // 
+        // 1980437560 is right!
     }
 
     public static void RunPart2()
     {
+        int part2sum = 0;
+
         ParseFile();
+        
+        foreach(int[] seq in sequences)
+        {
+            int nextNumber = DeterminePreviousNumber(seq);
+            part2sum += nextNumber;
+            Console.WriteLine("Previous number: " + nextNumber);
+        }
+
+        Console.WriteLine("Part 2 : " + part2sum);
+        // 977 is right!
     }
 }
