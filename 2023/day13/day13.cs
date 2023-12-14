533a3ead156016104de4fb7c4c88f8ab569c72a9
Matt Connolly <themattconnolly@gmail.com>
2023-12-14 08:48:39 -0500

    2023 day 13 part 1 ⭐

--- 2023/day13/day13.cs
+++ 2023/day13/day13.cs
@@ -44,12 +44,10 @@ public class Day13
         file.Close();
 
         foreach(Pattern pattern in Patterns)
-        {
-            char[] placeHolderColumn = new char[pattern.Rows.Count];
-            
+        {   
             for(int i = 0; i < pattern.Rows[0].Length; i++)
             {
-                pattern.Columns.Add(placeHolderColumn);
+                pattern.Columns.Add(new char[pattern.Rows.Count]);
             }
 
             Console.WriteLine("Pattern:");
@@ -58,7 +56,7 @@ public class Day13
                 // write the contents of the row to the console
                 Console.WriteLine((rowIndex + 1) + ": " + string.Join("", pattern.Rows[rowIndex]));
 
-                for(int columnIndex = 0; columnIndex < pattern.Rows[rowIndex].Length; columnIndex++)
+                for(int columnIndex = 0; columnIndex < pattern.Columns.Count; columnIndex++)
                 {
                     pattern.Columns[columnIndex][rowIndex] = pattern.Rows[rowIndex][columnIndex];
                 }
@@ -72,21 +70,70 @@ public class Day13
     {
         ParseFile();
 
+        long part1sum = 0;
         foreach(Pattern pattern in Patterns)
         {
             // look for two successive rows that are the same
-            bool foundMatch = false;
-            for(int rowIndex = 0; rowIndex < pattern.Rows.Count - 1; rowIndex++)
+            int horizontalMatch = GetPatternMatchResult(pattern.Rows);
+            if(horizontalMatch != -1)
             {
-                if(pattern.Rows[rowIndex].SequenceEqual(pattern.Rows[rowIndex + 1]))
-                {
-                    Console.WriteLine("Found a match at row " + (rowIndex + 1));
-                }
+                part1sum += 100 * horizontalMatch;
+                Console.WriteLine("Found a horizontal match at row " + horizontalMatch);
+            } else {
+                Console.WriteLine("No horizontal match");
             }
+            int verticalMatch = GetPatternMatchResult(pattern.Columns);
+            if(verticalMatch != -1)
+            {
+                part1sum += verticalMatch;
+                Console.WriteLine("Found a vertical match at column " + verticalMatch);
+            } else {
+                Console.WriteLine("No vertical match");}
         } 
 
-        //Console.WriteLine("Part 1 : " + part1sum);
-        //
+        Console.WriteLine("Part 1 : " + part1sum);
+        // 36041 is right!
+    }
+
+    private static int GetPatternMatchResult(List<char[]> rowOrColumns)
+    {
+        int result = -1;
+        for(int rowIndex = 0; rowIndex < rowOrColumns.Count - 1; rowIndex++)
+        {
+            if(rowOrColumns[rowIndex].SequenceEqual(rowOrColumns[rowIndex + 1]))
+            {
+                Console.WriteLine("Found a match at rows " + rowIndex + " and " + (rowIndex + 1));
+
+                int rowsFromStart = rowIndex;
+                int rowsFromEnd = rowOrColumns.Count - rowIndex - 2;
+
+                int rowsToCheck = Math.Min(rowsFromStart, rowsFromEnd);
+                bool foundMismatch = false;
+                for(int i = 1; i <= rowsToCheck; i++)
+                {
+                    int indexToCompare1 = rowIndex - i;
+                    int indexToCompare2 = rowIndex + i + 1;
+                    if(rowOrColumns[indexToCompare1].SequenceEqual(rowOrColumns[indexToCompare2]))
+                    {
+                        //Console.WriteLine("Found a match at rows " + (indexToCompare1) + " and " + (indexToCompare2));
+                    }
+                    else
+                    {
+                        Console.WriteLine("- Didn't match");
+                        foundMismatch = true;
+                        break;
+                    }
+                }
+
+                if(!foundMismatch)
+                {
+                    Console.WriteLine("- Matched all the way to the end");
+                    return rowIndex + 1; // this is the number of rows from the beginning
+                }
+            }
+        }
+
+        return result;
     }
     
     public static void RunPart2()
