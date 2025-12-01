461e2497e5115c3049e0d478b46e91c3b58d646d
Matt Connolly <themattconnolly@gmail.com>
2025-12-01 15:56:02 -0500

    2025 setup

---
+++ 2025/Program.cs
@@ -0,0 +1,32 @@
+// See https://aka.ms/new-console-ddtemplate for more information
+// cycle through the args
+using System.Reflection;
+
+if (args.Length > 0)
+{
+    // call a class method dynamically based on the first argument
+    if(args[0].StartsWith("day"))
+    {
+        Console.WriteLine(args[0]);
+        // get the day number
+        string dayNumber = args[0].Substring(3);
+        // get the class name
+        string className = "_2025.Day" + dayNumber;
+        // get the type
+        Type type = Type.GetType(className);
+        if(args[1].StartsWith("part")){
+            string partNumber = args[1].Substring(4);
+            // get the method
+            MethodInfo method = type.GetMethod("RunPart" + partNumber);
+            // create an instance of the class
+            object instance = Activator.CreateInstance(type);
+            // invoke the method
+            method.Invoke(instance, null);
+        }
+    }
+
+} else
+{
+    Console.WriteLine("No args");
+}
+
---
+++ 2025/day1/day1.cs
@@ -0,0 +1,22 @@
+namespace _2025;
+public class Day1
+{
+    private static void ParseFile()
+    {
+        string filename = "day1/input.txt";
+        // TODO: Parse input file
+    }
+
+    public static void RunPart1()
+    {
+        ParseFile();
+        Console.WriteLine("Part 1 : Not implemented yet");
+    }
+
+    public static void RunPart2()
+    {
+        ParseFile();
+        Console.WriteLine("Part 2 : Not implemented yet");
+    }
+}
+
