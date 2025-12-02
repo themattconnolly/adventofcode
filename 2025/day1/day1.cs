4c48a5125d95e1a61c410203c35452a6a1541cc6
Matt Connolly <themattconnolly@gmail.com>
2025-12-02 10:05:52 -0500

    2025 day 1 part 1 ⭐

--- 2025/day1/day1.cs
+++ 2025/day1/day1.cs
@@ -1,16 +1,55 @@
 namespace _2025;
 public class Day1
 {
+    // initialize dial
+    public static int dial = 50;
+    private static List<string> instructions = new List<string>();
     private static void ParseFile()
     {
         string filename = "day1/input.txt";
-        // TODO: Parse input file
+        string line;
+        System.IO.StreamReader file = new System.IO.StreamReader(filename);
+        while((line = file.ReadLine()) != null)
+        {
+            instructions.Add(line);
+        }
+        file.Close();
+    }
+
+    private static void TurnDial(string instruction)
+    {
+        // instruction looks like this: "L13" or "R7"
+        char direction = instruction[0];
+        int amount = int.Parse(instruction.Substring(1));
+        if (direction == 'L')
+        {
+            dial -= amount;
+        }
+        else if (direction == 'R')
+        {
+            dial += amount;
+        }
+
+        // wrap around dial, fix if negative
+        dial = ((dial % 100) + 100) % 100;
     }
 
     public static void RunPart1()
     {
         ParseFile();
-        Console.WriteLine("Part 1 : Not implemented yet");
+
+        int numberOfZeros = 0;
+        foreach (string instruction in instructions)
+        {
+            TurnDial(instruction);
+            Console.WriteLine("Dial is now at: " + dial);
+            if (dial == 0)
+            {
+                numberOfZeros++;
+            }
+        }
+
+        Console.WriteLine("Part 1 : " + numberOfZeros);
     }
 
     public static void RunPart2()
